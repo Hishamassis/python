4733980f53a529210fb76f939b4290125f1bb270
Hishamassis <hishamassiz.22@gmail.com>
2024-08-13 13:34:30 +0530

    python

---
+++ operators.py
@@ -0,0 +1,67 @@
+# ARITHMETIC OPERATORS
+
+# # addition
+# print(55+45)
+# print('hisham '+' assis ')
+# # substraction
+# print(88-44)
+# # division
+# print(100/4)
+# # multiplication
+# print(10*6)
+# print('hisham ' *5)
+# # floordivision
+# print(40//3)
+# print(40/3)
+# # exponential
+# print(3**3)
+# print()
+# # modulus
+# print(55%4)
+
+# ASSINGMENT OPERATORS
+
+# +=
+# a=40
+# a=a+10
+# print(a)
+# -=
+# a=40
+# a=a-5
+# print(a)
+# *=
+# a=40
+# a=a*3
+# print(a)
+# /=
+# a=40
+# a=a/5
+# print(a)
+# %=
+# a=40
+# a=a%9
+# print(a)
+# //=
+# a=40
+# a=a//5
+# print(a)
+# **=
+# a=4
+# a=a**2
+# print(a)
+# COMPARISON OPERATOR= ==,!=,<,>,<=,>=
+# ==
+# print(20==20)
+# print(20==2)
+# !=
+# print(10!=11)
+# print(20!=20)
+# <
+# print(10<5)
+# print(10<15)
+# >
+# print(10>5)
+# print(10>15)
+# <=
+# print(10<=5)
+# print(10<=15)
