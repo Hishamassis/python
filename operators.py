741cc9459d753ad2655fa0036b635a1a7a7d1748
Hishamassis <hishamassiz.22@gmail.com>
2024-08-14 13:29:38 +0530

    python

--- operators.py
+++ operators.py
@@ -1,67 +1,154 @@
-# ARITHMETIC OPERATORS
+                                                        # ARITHMETIC OPERATORS
 
 # # addition
+
 # print(55+45)
 # print('hisham '+' assis ')
+
 # # substraction
+
 # print(88-44)
+
 # # division
+
 # print(100/4)
+
 # # multiplication
+
 # print(10*6)
 # print('hisham ' *5)
+
 # # floordivision
+
 # print(40//3)
 # print(40/3)
+
 # # exponential
+
 # print(3**3)
 # print()
+
 # # modulus
+
 # print(55%4)
 
-# ASSINGMENT OPERATORS
+                                                        # ASSINGMENT OPERATORS
 
 # +=
+
 # a=40
 # a=a+10
 # print(a)
+
 # -=
+
 # a=40
 # a=a-5
 # print(a)
+
 # *=
+
 # a=40
 # a=a*3
 # print(a)
+
 # /=
+
 # a=40
 # a=a/5
 # print(a)
+
 # %=
+
 # a=40
 # a=a%9
 # print(a)
+
 # //=
+
 # a=40
 # a=a//5
 # print(a)
+
 # **=
+
 # a=4
 # a=a**2
 # print(a)
+
 # COMPARISON OPERATOR= ==,!=,<,>,<=,>=
+
 # ==
+
 # print(20==20)
 # print(20==2)
+
 # !=
+
 # print(10!=11)
 # print(20!=20)
+
 # <
+
 # print(10<5)
 # print(10<15)
+
 # >
+
 # print(10>5)
 # print(10>15)
+
 # <=
+
 # print(10<=5)
 # print(10<=15)
+
+                                                            # LOGICAL OPERATOR :and,or,not
+                                                                  
+# print(10==10 and 10>=12)
+    # true       true=true
+    # false      true=false
+    # false      false=false
+    # true       false=false
+
+# print(10==8 or 10>=11)
+      # true       true=true
+      # true       false=true
+      # false      true=true
+      # false      false=false
+
+# print(not(10>12))
+                                                                # MEMBERSHIP OPERATORS:in,not in
+
+# in
+
+# a='Hisham'
+# h='e'
+# print('e' in a)
+
+# not in
+# a=[4,8,9,7]
+# b=[[1,2,3,5,6] [4,8]]
+# print(b not in a)
+                                                                 #  IDENTIFY OPERATOR:is,isnot
+#  is                                                               
+
+# a=2 
+# b=2
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+# is not
+
+# a=2 
+# b=2
+# print(id(a))
+# print(id(b))
+# print(a is not b)
+
+                                                                     #  STRING
+# set of characters is string 
+
+
+
---
+++ string.py
@@ -0,0 +1,10 @@
+# a="python"
+# print(type(a))
+# b='[1,2,345]'
+# print(type(b))
+# c=" 'python' programs"
+# print(c)
+
+print('pyt\nhon','\tjava')
+a="python"
+print("lang = ",a)
